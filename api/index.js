b23e44dfc2a00141c840e6c823cb58941dee9d9f
Vishal <vishalqw78@gmail.com>
2023-08-20 15:22:13 +0530

    Routes Added

--- api/index.js
+++ api/index.js
@@ -7,6 +7,58 @@ import processDevFeed from '../feeds/processDevFeed.js';
 const app = express();
 const PORT = 3000;
 // Define a /bog route that accepts for blog card
+app.use((req, res, next) => {
+  if (req.url === '/') {
+      res.redirect('/blog');
+  } else {
+      next();
+  }
+});
+app.get('/blog-data', async (req, res) => {
+  try {
+    // Get parameters from the query string
+    let { theme = 'default', blogname = 'medium', index = 1, username = 'vishalqw78' } = req.query;
+    let Article;
+
+    if (blogname === 'medium') {
+      try {
+          Article = await processMediumFeed(username);
+        } catch (error) {
+          return res.status(500).send('Username/blogname does not exist or match with site name');
+        }
+    } else if (blogname === 'dev') {
+      try {
+          Article = await processDevFeed(username);
+        } catch (error) {
+          return res.status(500).send('Username/blogname does not exist or match with site name');
+        }
+    } else if (blogname === 'hashnode') {
+      try {
+          Article = await processHashnodeFeed(username);
+        } catch (error) {
+          return res.status(500).send('Username/blogname does not exist or match with site name');
+        }
+    } else {
+      return res.status(400).send('Invalid blogname');
+    }
+
+    const maxsize = Article.length;
+
+    if (index > maxsize || index < 1) {
+      index = 1;
+    }
+    let i=index-1;
+    //destructure the Article at ith index
+    const { url, title, date, author, thumbnail, description } = Article[i];
+    //for sending the file as a data
+    const contentdata = {theme, blogname, url, title, date, author, description};
+    res.status(200).json(contentdata);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Internal Server Error');
+  }
+
+});
 app.get('/blog', async (req, res) => {
     try {
       // Get parameters from the query string
@@ -53,6 +105,11 @@ app.get('/blog', async (req, res) => {
       res.status(500).send('Internal Server Error');
     }
   });
+
+  
+app.all('*', (req, res, next) => {
+  res.redirect('/blog');
+});
 
 // Start the server
 app.listen(PORT, () => {
